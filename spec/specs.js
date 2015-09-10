1a76e7674a819322336c6328d4b876a2ef91c235
Don Schemmel <dons500@gmail.com>
2015-09-10 12:13:56 -0700

    Second test pass

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('countUp', function() {
   it("Counts up to 10 by multiples of 1", function() {
     expect(countUp(1, 10)).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
   });
+
+  it("Counts up to 10 by multiples of 2", function() {
+    expect(countUp(2, 10)).to.eql([2, 4, 6, 8, 10])
+  });
 });
